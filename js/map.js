33118ef9432e2634ba287545fa4a6f7a56e70ab0
o.bogatyreva <bogatireva.o.v@gmail.com>
2022-07-13 22:59:19 +0300

    add main marker

--- js/map.js
+++ js/map.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
       activeStatePage();
     })
     .setView({
-      lat: 59.92749,
-      lng: 30.31127,
+      lat: 35.4122,
+      lng: 139.2530,
     }, 10);
 
   L.tileLayer(
@@ -16,4 +16,27 @@ document.addEventListener('DOMContentLoaded', () => {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     },
   ).addTo(map);
+
+  const mainPinIcon = L.icon({
+    iconUrl: './img/main-pin.svg',
+    iconSize: [52, 52],
+    iconAnchor: [26, 52],
+  });
+
+  const marker = L.marker(
+    {
+      lat: 35.4200,
+      lng: 139.2530,
+    },
+    {
+      draggable: true,
+      icon: mainPinIcon,
+    },
+  );
+
+  marker.on('moveend', (evt) => {
+    evt.target.getLatLng();
+  });
+
+  marker.addTo(map);
 });
