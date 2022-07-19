614232344e41cea1a874ee91fbfbfefff8588816
o.bogatyreva <bogatireva.o.v@gmail.com>
2022-07-19 11:52:56 +0300

    change address from moved marker in map

--- js/map.js
+++ js/map.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
   );
 
   marker.on('moveend', (evt) => {
-    evt.target.getLatLng();
+    document.querySelector('#address').value = `${evt.target.getLatLng().lat.toFixed(4)} ${evt.target.getLatLng().lng.toFixed(4)}`;
   });
 
   marker.addTo(map);
